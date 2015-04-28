e1600b442087e6903d63cbcdb22d5f22a0e1c816
Irene1612 <nyuron@172.16.159.26>
2015-04-28 12:14:13 +0200

    Change a name class and add excel

--- src/test/java/spikeTest/EjemploTest.java
+++ src/test/java/spikeTest/EjemploTest.java
@@ -5,7 +5,7 @@ import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import spike.Ejemplo;
+import spike.Example;
 
 
 public class EjemploTest {
@@ -25,6 +25,7 @@ public class EjemploTest {
     @Test
     public void testEjemplo() {
         while(data.hasNext()){
+            data.getEjemplo();
             fail("Not yet implemented");
             data.next();
         }
@@ -33,40 +34,58 @@ public class EjemploTest {
     @Test
     public void testM1() {
         while(data.hasNext()){
-            Ejemplo ejemplo = new Ejemplo();
-            assertEquals(data.getM1Result(), ejemplo.m1());
+            assertEquals(data.getM1Result(), data.getEjemplo().m1());
             data.next();
         }
     }
 
     @Test
     public void testM1Int() {
-        fail("Not yet implemented");
+        while(data.hasNext()){
+            assertEquals(data.getM1IntResult(), data.getEjemplo().m1(data.getM1IntX()));
+            data.next();
+        }
     }
 
     @Test
     public void testM1Float() {
-        fail("Not yet implemented");
+        while(data.hasNext()){
+            assertEquals(data.getM1FloatResult(), data.getEjemplo().m1(data.getM1FloatX()));
+            data.next();
+        }
     }
 
     @Test
     public void testM1Double() {
-        fail("Not yet implemented");
+        while(data.hasNext()){
+            assertEquals(data.getM1DoubleResult(), data.getEjemplo().m1(data.getM1DoubleX()));
+            data.next();
+        }
     }
 
     @Test
     public void testM1Boolean() {
-        fail("Not yet implemented");
+        while(data.hasNext()){
+            assertEquals(data.getM1BooleanResult(), data.getEjemplo().m1(data.getM1BooleanX()));
+            data.next();
+        }
     }
 
     @Test
     public void testM1String() {
-        fail("Not yet implemented");
+        while(data.hasNext()){
+            assertEquals(data.getM1StringResult(), data.getEjemplo().m1(data.getM1StringX()));
+            data.next();
+        }
     }
 
     @Test
     public void testM1IntInt() {
-        fail("Not yet implemented");
+        while(data.hasNext()){
+            assertEquals(data.getM1IntIntResult(), data.getEjemplo().m1(data.getM1IntIntX(), data.getM1IntIntY()));
+            data.next();
+        }
     }
-
+    
+    
 }
--- src/test/java/spikeTest/EjemploTestData.java
+++ src/test/java/spikeTest/EjemploTestData.java
@@ -1,32 +1,87 @@
 package spikeTest;
-import spike.DataAccess;
 
+import spike.DataAccess;
+import spike.Example;
 
 public class EjemploTestData {
-    private DataAccess data;
+    private DataAccess dataAccess;
+
     private String excel;
-    
+
     public EjemploTestData() {
         excel = "EjemploTestData.xls";
     }
-
-    public int getM1IntX(){
-        return data.getInt();
-    }
-
+    
     public void reset() {
-        data.reset();        
+        dataAccess.reset();
     }
 
     public boolean next() {
-        return data.next();
+        return dataAccess.next();
     }
 
     public boolean hasNext() {
-        return data.hasNext();
+        return dataAccess.hasNext();
+    }
+    
+    public Example getEjemplo() {
+        return null;
     }
 
     public int getM1Result() {
-        return 0;
+        return dataAccess.getInt("getM1Result");
     }
+
+    public int getM1IntX() {
+        return dataAccess.getInt("getM1IntX");
+    }       
+
+    public int getM1IntResult() {
+        return dataAccess.getInt("getM1IntResult");
+    }
+
+    public int getM1FloatResult() {
+        return dataAccess.getInt("getM1FloatResult");
+    }
+
+    public float getM1FloatX() {
+        return dataAccess.getFloat("getM1FloatX");
+    }
+
+    public int getM1DoubleResult() {
+        return dataAccess.getInt("getM1DoubleResult");
+    }
+
+    public double getM1DoubleX() {
+        return dataAccess.getDouble("getM1DoubleX");
+    }
+
+    public int getM1BooleanResult() {
+        return dataAccess.getInt("getM1BooleanResult");
+    }
+
+    public boolean getM1BooleanX() {
+        return dataAccess.getBoolean("getM1BooleanX");
+    }
+
+    public int getM1StringResult() {
+        return dataAccess.getInt("getM1BooleanResult");
+    }
+
+    public String getM1StringX() {
+        return dataAccess.getString("getM1StringX");
+    }
+
+    public int getM1IntIntResult() {
+        return dataAccess.getInt("getM1IntIntResult");
+    }
+
+    public int getM1IntIntX() {
+        return dataAccess.getInt("getM1IntIntX");
+    }
+
+    public int getM1IntIntY() {
+        return dataAccess.getInt("getM1IntIntY");
+    }
+
 }
