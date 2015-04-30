7ecaaeef13f894875b9e375213b14a88b8097b5c
Irene1612 <nyuron@172.16.159.26>
2015-04-30 12:55:22 +0200

    Refactor method hasNext.

--- src/main/java/spike/generated/ExampleTestDataRetriever.java
+++ src/main/java/spike/generated/ExampleTestDataRetriever.java
@@ -2,9 +2,9 @@ package spike.generated;
 
 import spike.Example;
 import spike.TestDataRetriever;
-import spike.error.EmptyDataAccessError;
-import spike.error.InvalidDataAccessError;
-import spike.error.TypeDataAccessError;
+import spike.error.EmptyDataReaderErrorException;
+import spike.error.InvalidDataReaderErrorException;
+import spike.error.TypeDataReaderErrorException;
 
 public class ExampleTestDataRetriever extends TestDataRetriever {
 
@@ -15,65 +15,48 @@ public class ExampleTestDataRetriever extends TestDataRetriever {
     }
 
     public boolean hasNext() {
-        return this.getDataAccess().hasNext();
+        return this.getDataReader().hasNext();
     }
 
     public boolean hasNext(int constructMode) {
         boolean captured = false;
         switch (constructMode) {
-        case 0:
+        case 0:            
             do {
                 try {
-                    String x = this.getDataAccess().getString("getExample");
-                    this.example = new Example();
-                    if (x != "x" || x != "X") {
-                        throw new InvalidDataAccessError();
-                    }
-                    captured = true;
-                } catch (EmptyDataAccessError e) {
-                    this.next();
-                } catch (TypeDataAccessError e) {
-                    System.out.println("Ha introducido un valor de un tipo diferente al esperado en la fila "
-                            + this.getDataAccess().getRow() + " - columna " + this.getDataAccess().getColumn()
+                    captured = this.tryCase0();                    
+                } catch (EmptyDataReaderErrorException e) {
+                    this.emptyDataReaderErrorCatch();                    
+                } catch (TypeDataReaderErrorException e) {
+                    this.typeDataReaderErrorCatch("Ha introducido un valor de un tipo diferente al esperado en la fila "
+                            + this.getDataReader().getRow() + " - columna " + this.getDataReader().getColumn()
                             + ". Se esperaba un carácter 'x'.");
-                    System.exit(0);
-                } catch (InvalidDataAccessError e) {
-                    System.out.println("Debe introducir 'x' o 'X' en la fila " + this.getDataAccess().getRow() + " - columna "
-                            + this.getDataAccess().getColumn() + ".");
-                    System.exit(0);
                 }
             } while (!captured);
             return true;
         case 1:
             do {
-                try {
-                    int value1 = this.getDataAccess().getInt("getExampleIntValue1");
-                    this.example = new Example(value1);
-                    captured = true;
-                } catch (EmptyDataAccessError e) {
-                    this.next();
-                } catch (TypeDataAccessError e) {
-                    System.out.println("Ha introducido un valor de un tipo diferente al esperado en la fila "
-                            + this.getDataAccess().getRow() + " - columna " + this.getDataAccess().getColumn()
+                try {                    
+                    captured = this.tryCase1();
+                } catch (EmptyDataReaderErrorException e) {
+                    this.emptyDataReaderErrorCatch();
+                } catch (TypeDataReaderErrorException e) {
+                    this.typeDataReaderErrorCatch("Ha introducido un valor de un tipo diferente al esperado en la fila "
+                            + this.getDataReader().getRow() + " - columna " + this.getDataReader().getColumn()
                             + ". Se esperaba un número entero.");
-                    System.exit(0);
                 }
             } while (!captured);
             return true;
         case 2:
             do {
-                try {
-                    int value1 = this.getDataAccess().getInt("getExampleIntIntValue1");
-                    int value2 = this.getDataAccess().getInt("getExampleIntIntValue2");
-                    this.example = new Example(value1, value2);
-                    captured = true;
-                } catch (EmptyDataAccessError e) {
-                    this.next();
-                } catch (TypeDataAccessError e) {
-                    System.out.println("Ha introducido un valor de un tipo diferente al esperado en la fila "
-                            + this.getDataAccess().getRow() + " - columna " + this.getDataAccess().getColumn()
+                try {                    
+                    captured = this.tryCase2();                    
+                } catch (EmptyDataReaderErrorException e) {
+                    this.emptyDataReaderErrorCatch();
+                } catch (TypeDataReaderErrorException e) {
+                    this.typeDataReaderErrorCatch("Ha introducido un valor de un tipo diferente al esperado en la fila "
+                            + this.getDataReader().getRow() + " - columna " + this.getDataReader().getColumn()
                             + ". Se esperaba un número entero.");
-                    System.exit(0);
                 }
             } while (!captured);
             return true;
@@ -81,15 +64,41 @@ public class ExampleTestDataRetriever extends TestDataRetriever {
             System.out.println("Construct mode error in hasnest(int)");
             return false;
         }
+    }
 
+    private boolean tryCase0() throws EmptyDataReaderErrorException, TypeDataReaderErrorException {
+        String x = this.getDataReader().getString("getExample");
+        this.example = new Example();
+        if (x != "x" || x != "X") {
+            try {
+                throw new InvalidDataReaderErrorException();
+            } catch (InvalidDataReaderErrorException e) {
+                super.invalidDataReaderErrorCatch("Debe introducir 'x' o 'X' en la fila " + this.getDataReader().getRow() + " - columna "
+                        + this.getDataReader().getColumn() + ".");
+            }
+        }
+        return true;        
+    }
+    
+    private boolean tryCase1() throws EmptyDataReaderErrorException, TypeDataReaderErrorException {
+        int value1 = this.getDataReader().getInt("getExampleIntValue1");
+        this.example = new Example(value1);
+        return true;
+    }
+    
+    private boolean tryCase2() throws EmptyDataReaderErrorException, TypeDataReaderErrorException {
+        int value1 = this.getDataReader().getInt("getExampleIntIntValue1");
+        int value2 = this.getDataReader().getInt("getExampleIntIntValue2");
+        this.example = new Example(value1, value2);
+        return true;
     }
 
     public Example getExample() {
         return example;
     }
 
-    public int getConstructMode() {
-        return this.getDataAccess().getConstructMode();
+    private int getConstructMode() {
+        return this.getDataReader().getConstructMode();
     }
 
     public int getM1Result() {
