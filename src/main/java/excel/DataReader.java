e4458ccd1eb49bb1ad0837ab93b70a83ea167796
cdavidrp <cdavidrp@gmail.com>
2015-04-30 12:16:11 +0200

    Update DataReader to read the headers and store their respective values

--- src/main/java/excel/DataReader.java
+++ src/main/java/excel/DataReader.java
@@ -2,12 +2,13 @@ package excel;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Iterator;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -17,73 +18,58 @@ import spike.error.TypeDataAccessError;
 
 public class DataReader {
 
-    private String fileName;
-
     private XSSFWorkbook workbook;
 
     private XSSFSheet currentSheet;
 
     private XSSFRow currentRow;
 
-    private XSSFCell currentCell;
+    private Iterator<Row> rowIterator;
 
-    private static final int STARTING_ROW = 0;
+    private HashMap<String, Integer> headers;
 
-    private static final int STARTING_CELL = 0;
-
-    public DataReader(String fileName) {
-        this.fileName = fileName;
+    public DataReader(String filePath) {
+        FileInputStream fis;
         try {
-            this.workbook = new XSSFWorkbook(fileName);
-        } catch (IOException e1) {
-            System.out.println("Error en la lectura del fichero " + fileName);
+            fis = new FileInputStream(new File(filePath));
+            this.workbook = new XSSFWorkbook(fis);
+        } catch (FileNotFoundException e) {
+            System.out.println("Fichero " + filePath + " no encontrado");
             System.exit(0);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         this.currentSheet = this.workbook.getSheetAt(0);
-        this.currentRow = this.currentSheet.createRow(STARTING_ROW);
-        this.currentCell = this.currentRow.createCell(STARTING_CELL);
-        try {
-            XSSFRow row;
-            FileInputStream fis = new FileInputStream(new File("Example.xlsx"));
-            XSSFWorkbook workbook = new XSSFWorkbook(fis);
-            XSSFSheet spreadsheet = workbook.getSheetAt(0);
-            Iterator<Row> rowIterator = spreadsheet.iterator();
-            while (rowIterator.hasNext()) {
-                row = (XSSFRow) rowIterator.next();
-                Iterator<Cell> cellIterator = row.cellIterator();
-                while (cellIterator.hasNext()) {
-                    Cell cell = cellIterator.next();
-                    switch (cell.getCellType()) {
-                    case Cell.CELL_TYPE_NUMERIC:
-                        System.out.print(cell.getNumericCellValue() + " \t\t ");
-                        break;
-                    case Cell.CELL_TYPE_STRING:
-                        System.out.print(cell.getStringCellValue() + " \t\t ");
-                        break;
-                    }
-                }
-                System.out.println();
-            }
-            fis.close();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        this.rowIterator = this.currentSheet.iterator();
+        this.next();
+        this.fillHeaders();
+    }
+
+    private void fillHeaders() {
+        this.headers = new HashMap<String, Integer>();
+        for (Cell cell : this.currentRow) {
+            this.headers.put(cell.getStringCellValue(), cell.getColumnIndex());
         }
     }
 
     public void reset() {
-        // TODO Auto-generated method stub
-
+        this.rowIterator = this.currentSheet.iterator();
     }
 
     public boolean next() {
-        // TODO Auto-generated method stub
-        return false;
+        boolean result;
+        if (result = this.hasNext()) {
+            this.currentRow = (XSSFRow) this.rowIterator.next();
+        } 
+        return result;
     }
 
     public boolean hasNext() {
-        // TODO Auto-generated method stub
-        return false;
+        return this.rowIterator.hasNext();
+    }
+
+    public HashMap<String, Integer> getHeaders() {
+        return headers;
     }
 
     public String getRow() {
