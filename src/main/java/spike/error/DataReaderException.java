ce083ef12ee65e3688b46e8177ac63024f939999
cdavidrp <cdavidrp@gmail.com>
2015-05-05 14:16:44 +0200

    Add default constructor

--- src/main/java/spike/error/DataReaderException.java
+++ src/main/java/spike/error/DataReaderException.java
@@ -4,9 +4,11 @@ public class DataReaderException extends Exception {
 
     private static final long serialVersionUID = 1L;
 
+    public DataReaderException() {
+    }
+    
     public DataReaderException(String message) {
         super(message);
     }
-
     
 }
--- src/main/java/spike/error/InvalidDataReaderException.java
+++ src/main/java/spike/error/InvalidDataReaderException.java
@@ -4,6 +4,10 @@ public class InvalidDataReaderException extends DataReaderException {
 
     private static final long serialVersionUID = 1L;
 
+    public InvalidDataReaderException() {
+        super();
+    }
+    
     public InvalidDataReaderException(String message) {
         super(message);
     }
