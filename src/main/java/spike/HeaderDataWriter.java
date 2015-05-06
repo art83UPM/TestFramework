8fe29e140feeaf2f89046ce5a93d9b3fd79f1b55
Irene1612 <nyuron@172.16.159.26>
2015-05-06 13:10:49 +0200

    Add constructor and methods to HeaderDataWriter.java

--- src/main/java/spike/HeaderDataWriter.java
+++ src/main/java/spike/HeaderDataWriter.java
@@ -1,5 +1,49 @@
 package spike;
 
-public class HeaderDataWriter {
+import excel.DataWriter;
+
+public class HeaderDataWriter implements Visitor {
+
+    private DataWriter dataWriter;
+
+    private String path;
+
+    public HeaderDataWriter(String path) {
+        this.path = path;
+    }
+
+    public void visit(Clazz clazz) {
+        this.dataWriter = new DataWriter(path + "\\" + clazz.getName() + "TestData.xlsx");
+    }
+
+    public void visit(ConstructorMember constructorMember) {
+        String header = "get" + constructorMember.getName();
+        if (constructorMember.getParametersType().size() == 0) {
+            dataWriter.write(header);
+        } else {
+            for (ParameterMember parameterMember : constructorMember.getParametersType()) {
+                header += parameterMember.getType();
+            }
+            for (int i = 0; i < constructorMember.getParametersType().size(); i++) {
+                dataWriter.write(header + "Parameter" + i);
+            }
+        }
+
+    }
+
+    public void visit(MethodMember methodMember) {
+        String header = "get" + methodMember.getName();
+        for (ParameterMember parameterMember : methodMember.getParametersType()) {
+            header += parameterMember.getType();
+        }
+        for (int i = 0; i < methodMember.getParametersType().size(); i++) {
+            dataWriter.write(header + "Parameter" + i);
+        }
+        dataWriter.write(header + "Result");
+    }
+
+    public void close() {
+        dataWriter.save();
+    }
 
 }
--- src/main/java/spike/TestGenerator.java
+++ src/main/java/spike/TestGenerator.java
@@ -7,8 +7,11 @@ public class TestGenerator {
             ClazzReader clazzReader = new ClazzReader(Class.forName(clazzName));
             Clazz clazz = clazzReader.read();
             TestWriter testWriter = new TestWriter("C:\\Users\\nyuron\\Desktop");
+            HeaderDataWriter headerDataWriter = new HeaderDataWriter("C:\\Users\\nyuron\\Desktop");
             clazz.accept(testWriter);
+            clazz.accept(headerDataWriter);
             testWriter.close();
+            headerDataWriter.close();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
             System.exit(0);
