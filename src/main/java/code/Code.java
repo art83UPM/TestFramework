10e2c66a5c6737d6797f67c9003cdc9df4550fa6
cdavidrp <cdavidrp@gmail.com>
2015-05-07 13:29:24 +0200

    Add Code.java

---
+++ src/main/java/code/Code.java
@@ -0,0 +1,28 @@
+package code;
+
+import java.io.File;
+import java.util.List;
+
+public class Code {
+
+    private List<CodeComponent> components;
+    
+    private File file;
+    
+    public Code(String path) {
+        this.file = new File(path);
+        this.inspect();
+    }
+    
+    private void inspect() {
+        for (File file : this.file.listFiles()) {
+            if (file.isDirectory()) {
+                this.add(new Package(file));
+            }
+        }
+    }
+
+    private void add(CodeComponent component) {
+        this.components.add(component);
+    }
+}
