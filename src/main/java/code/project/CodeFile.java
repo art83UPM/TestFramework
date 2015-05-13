86f1beb80faa36a4f597f54bbe10628cc3676751
Irene1612 <nyuron@172.16.159.26>
2015-05-13 14:21:30 +0200

    Change classes for a better display when testing. And for inclusion of the ProjectMember inheritance.

--- src/main/java/code/project/CodeFile.java
+++ src/main/java/code/project/CodeFile.java
@@ -2,4 +2,14 @@ package code.project;
 
 public abstract class CodeFile implements CodeComponent {
 
+    protected String name;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public abstract boolean exist(ProjectMember projectMember, ProjectClazz projectClazz, ProjectPackage projectPackage);
+
+    public abstract boolean exist(ProjectMember projectMember, ProjectClazz projectClazz);
+
 }
--- src/main/java/code/project/ProjectCode.java
+++ src/main/java/code/project/ProjectCode.java
@@ -4,27 +4,41 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import code.Margin;
+
 public class ProjectCode {
-    
+
     private File file;
-    
+
     private List<CodeFile> components;
-    
+
     public ProjectCode(String path) {
         this.file = new File(path);
         this.components = new ArrayList<CodeFile>();
         this.build();
     }
-    
+
     private void build() {
         for (File file : this.file.listFiles()) {
             if (file.isDirectory()) {
+                Margin.instance().inc();
                 this.add(new ProjectPackage(file));
+                Margin.instance().dec();
             }
         }
     }
 
     private void add(CodeFile component) {
         this.components.add(component);
+    }
+
+    public boolean exist(ProjectMember projectMember, ProjectClazz projectClazz, ProjectPackage projectPackage) {
+        for (CodeFile codeFile : components) {
+            if (codeFile.exist(projectMember, projectClazz, projectPackage)) {
+                System.out.println(Margin.instance().tabs() + "Sí está!");
+                return true;
+            }
+        }
+        return false;
     }
 }
