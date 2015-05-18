55176c675be809669e673ec5f1b6341c4d60e482
cdavidrp <cdavidrp@gmail.com>
2015-05-18 12:06:54 +0200

    Update exception to accept a message

--- src/main/java/exceptions/InvalidHeaderDataReaderException.java
+++ src/main/java/exceptions/InvalidHeaderDataReaderException.java
@@ -3,9 +3,10 @@ package exceptions;
 
 public class InvalidHeaderDataReaderException extends DataReaderException {
 
-    /**
-     * 
-     */
     private static final long serialVersionUID = 1L;
+
+    public InvalidHeaderDataReaderException(String message) {
+        super(message);
+    }
 
 }
