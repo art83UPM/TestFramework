a2c47e8582fe9a3708b7f5db4c2e23a0a179f07a
cdavidrp <cdavidrp@gmail.com>
2015-05-18 11:03:54 +0200

    Add new exception

---
+++ src/main/java/exceptions/InvalidHeaderDataReaderException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+
+public class InvalidHeaderDataReaderException extends DataReaderException {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+}
