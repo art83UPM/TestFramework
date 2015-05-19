22a528800ff57748defba6a3be3d377cf515446b
Irene1612 <nyuron@172.16.159.26>
2015-05-19 12:55:07 +0200

    Update constructor.

--- src/main/java/code/config/ConfigPackage.java
+++ src/main/java/code/config/ConfigPackage.java
@@ -1,13 +1,41 @@
 package code.config;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class ConfigPackage extends ConfigCodeFile{
-    private List<ConfigCodeFile> configCodeFileList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
-    public ConfigPackage(List<ConfigCodeFile> configCodeFileList) {
-        this.configCodeFileList = configCodeFileList;
-    } 
-    
-    
+public class ConfigPackage {
+    private JSONObject jsonPackage;
+
+    private String name;
+
+    private List<ConfigPackage> configPackagesList;
+
+    private List<ConfigClazz> configClazzesList;
+
+    public ConfigPackage(JSONObject jsonPackage) {        
+        this.jsonPackage = jsonPackage;
+        this.name = (String) this.jsonPackage.get("name");
+        this.configPackagesList = new ArrayList<ConfigPackage>();
+        this.configClazzesList = new ArrayList<ConfigClazz>();
+        this.build();
+    }
+
+    private void build() {
+        JSONArray packages = (JSONArray) this.jsonPackage.get("packages");
+        if (!packages.isEmpty()) {
+            for (Object jsonPackage : packages) {
+                this.configPackagesList.add(new ConfigPackage((JSONObject) jsonPackage));
+            }
+        }
+        JSONArray clazzes = (JSONArray) this.jsonPackage.get("classes");
+        if (!clazzes.isEmpty()) {
+            for (Object jsonClazz : clazzes) {
+                this.configClazzesList.add(new ConfigClazz((JSONObject) jsonClazz));
+            }
+        }
+    }
+
 }
