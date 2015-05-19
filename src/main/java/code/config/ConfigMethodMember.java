6546d0332c1b19c446ed049a828c0e5483d064de
cdavidrp <cdavidrp@gmail.com>
2015-05-19 10:43:10 +0200

    Fix errors

--- src/main/java/code/config/ConfigMethodMember.java
+++ src/main/java/code/config/ConfigMethodMember.java
@@ -1,5 +1,10 @@
 package code.config;
 
 public class ConfigMethodMember extends ConfigMember{
+
+    public ConfigMethodMember(String name, String state, String test) {
+        super(name, state, test);
+        // TODO Auto-generated constructor stub
+    }
     
 }
