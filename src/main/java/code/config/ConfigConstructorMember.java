e85e4252c5f0381e460f44728ecfcc04312d8bb1
Irene1612 <nyuron@172.16.159.26>
2015-05-20 14:11:13 +0200

    Add overridden equals method.

--- src/main/java/code/config/ConfigConstructorMember.java
+++ src/main/java/code/config/ConfigConstructorMember.java
@@ -3,9 +3,21 @@ package code.config;
 import org.json.simple.JSONObject;
 
 public class ConfigConstructorMember extends ConfigMember {
-    
+
     public ConfigConstructorMember(JSONObject jsonConstructor) {
         super(jsonConstructor);
     }
-   
+
+    @Override
+    public boolean equals(Object obj) {
+        if ((obj instanceof ConfigConstructorMember)) {
+            ConfigConstructorMember configConstructorMember = (ConfigConstructorMember) obj;
+            if(!this.name.equals(configConstructorMember.getName())){
+                return false;
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- src/main/java/code/config/ConfigMethodMember.java
+++ src/main/java/code/config/ConfigMethodMember.java
@@ -7,5 +7,18 @@ public class ConfigMethodMember extends ConfigMember{
     public ConfigMethodMember(JSONObject jsonMethod) {
        super(jsonMethod);
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if ((obj instanceof ConfigMethodMember)) {
+            ConfigMethodMember configMethodMember = (ConfigMethodMember) obj;
+            if(!this.name.equals(configMethodMember.getName())){
+                return false;
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
     
 }
