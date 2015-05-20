67fa8f41bbc65f65608e76f4420b6cc3031add64
Irene1612 <nyuron@172.16.159.26>
2015-05-20 14:09:04 +0200

    Add method getName.

--- src/main/java/code/config/ConfigMember.java
+++ src/main/java/code/config/ConfigMember.java
@@ -14,6 +14,8 @@ public class ConfigMember {
         this.status = (String) this.jsonMember.get("status");
         this.test = (String) this.jsonMember.get("test");
     }
-    
-    
+
+    public String getName() {
+        return name;
+    }   
 }
