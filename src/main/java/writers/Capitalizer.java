9ddb7a013d43e18a34abbda18b96e68ec98b15b6
Carlos David Rodríguez Peña <cdavidrp@gmail.com>
2015-06-14 20:32:30 +0200

    Add utility class to help with code generation

---
+++ src/main/java/writers/Capitalizer.java
@@ -0,0 +1,12 @@
+package writers;
+
+public class Capitalizer {
+	
+	public static String capitalize (String string) {
+		return string.substring(0, 1).toUpperCase() + string.substring(1);
+	}
+	
+	public static String unCapitalize (String string) {
+		return string.substring(0, 1).toLowerCase() + string.substring(1);
+	}
+}
